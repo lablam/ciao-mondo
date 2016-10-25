49a8bb95f5b2784b0b570e33f990cdcf0e9b8d2b
Giorgio <lablam@istitutobrunofranchetti.gov.it>
2016-10-25 12:57:57 +0200

    Create Natale.java

---
+++ Natale.java
@@ -0,0 +1,33 @@
+/*
+  Questo programma stampa un albero di Natale
+*/
+
+public class Natale {
+
+    public static void main(String[] a) {
+	int numPiani = 7;
+	for (int i = 0; i < numPiani; i++) {
+	    stampaSpazi(numPiani - i);
+	    stampaStelle(2*i - 1);
+	    System.out.println();
+	}
+	// stampa il tronco:
+	for (int i = 0; i < 2; i++) {
+	    stampaSpazi(numPiani - 2);
+	    stampaStelle(3);
+	    System.out.println();
+	}
+	System.out.println();
+    }
+
+    public static void stampaStelle(int numF) {
+	for (int i = 0; i < numF; i++) 
+	    System.out.print("*");
+    }
+
+    public static void stampaSpazi(int numSp) {
+	for (int i = 0; i < numSp; i++) 
+	    System.out.print(" ");
+    }
+
+}
